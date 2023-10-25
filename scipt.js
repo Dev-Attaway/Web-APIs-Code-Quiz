d4375e09bdea150e234e5fd6b67fe7de2e8b40d7
unknown <attaway.code@gmail.com>
2023-10-25 15:33:14 -0700

    js-html-css.2

--- scipt.js
+++ scipt.js
@@ -20,6 +20,7 @@ var lose = document.querySelector(".lose");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 
+
 let btn1 = document.getElementById("answer1");
 let btn2 = document.getElementById("answer2");
 let btn3 = document.getElementById("answer3");
@@ -34,26 +35,69 @@ var isWin = false;
 var timer;
 var timerCount;
 
-let questionCtr = 0;
+let quesCtr = 0;
+let correct;
 
 let quizQuestions = [
-    {
-        question: "funny monkey is",
-        answer1:"A.) poopy",
-        answer2:"B.) stinky",
-        answer3:"C.) Donkey Kong",
-        answer4:"D.) Funky Monkey Friday",
-        correctAnswer: "Donkey Kong"
-    }, 
-    
-    {
-        question: "aaaaaaaaaaaaaaaaaaa",
-        answer1:"sssssssssssss",
-        answer2:"ssssssssssssss",
-        answer3:"sssssssssssssssss",
-        answer4:"ssssssssssssssss",
-        correctAnswer: ""
-    }
+  {
+    question: "funny monkey is",
+    questionNum: 1,
+    answer1: "A.) poopy",
+    answer2: "B.) stinky",
+    answer3: "C.) Donkey Kong",
+    answer4: "D.) Funky Monkey Friday",
+    correctAnswer: "Donkey Kong"
+  },
+
+  {
+    question: "aaaaaaaaaaaaaaaaaaa",
+    questionNum: 2,
+    answer1: "sssssssssssss",
+    answer2: "ssssssssssssss",
+    answer3: "sssssssssssssssss",
+    answer4: "ssssssssssssssss",
+    correctAnswer: ""
+  },
+
+  {
+    question: "funny monkey is",
+    questionNum: 3,
+    answer1: "A.) poopy",
+    answer2: "B.) stinky",
+    answer3: "C.) Donkey Kong",
+    answer4: "D.) Funky Monkey Friday",
+    correctAnswer: "Donkey Kong"
+  },
+
+  {
+    question: "aaaaaaaaaaaaaaaaaaa",
+    questionNum: 4,
+    answer1: "sssssssssssss",
+    answer2: "ssssssssssssss",
+    answer3: "sssssssssssssssss",
+    answer4: "ssssssssssssssss",
+    correctAnswer: ""
+  },
+
+  {
+    question: "funny monkey is",
+    questionNum: 5,
+    answer1: "A.) poopy",
+    answer2: "B.) stinky",
+    answer3: "C.) Donkey Kong",
+    answer4: "D.) Funky Monkey Friday",
+    correctAnswer: "Donkey Kong"
+  },
+
+  {
+    question: "aaaaaaaaaaaaaaaaaaa",
+    questionNum: 6,
+    answer1: "sssssssssssss",
+    answer2: "ssssssssssssss",
+    answer3: "sssssssssssssssss",
+    answer4: "ssssssssssssssss",
+    correctAnswer: ""
+  }
 ]
 
 
@@ -61,7 +105,6 @@ let quizQuestions = [
 function init() {
   getWins();
   getlosses();
-  renderMessage();
 }
 
 // The startGame function is called when the start button is clicked
@@ -70,6 +113,11 @@ function startGame() {
   timerCount = 10;
   // Prevents start button from being clicked when round is in progress
   startButton.disabled = true;
+
+  // once start button is pressed, startgame is called and game begins, 
+  // questions are rendered on screen
+  show();
+  renderMessage();
   startTimer()
 }
 
@@ -92,10 +140,9 @@ function loseGame() {
 // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
 function startTimer() {
   // Sets timer
-  timer = setInterval(function() {
+  timer = setInterval(function () {
     timerCount--;
     timerElement.textContent = timerCount;
-
   }, 1000);
 }
 
@@ -145,51 +192,116 @@ function checkWin() {
   }
 }
 
-function renderMessage() 
-{
-    // questionIndex will determine which question object to ask the user 
-    let questionIndex = Math.floor(Math.random() * 2);
+function setCounter() {
+  quesCtr++;
+}
 
-    // Grabs the a random quiz question object made on line 33
-    var quiz = quizQuestions[questionIndex];
+function renderMessage() {
+  // questionIndex will determine which question object to ask the user 
 
-    // grabs variables from the object from the quizQuestions
-    // At this moment a quiz question and corresponding answers are loaded into the variables in lines
-    // 155 -159
-    var quizQues = quiz.question;
-    var quizAnswer1 = quiz.answer1;
-    var quizAnswer2 = quiz.answer2;
-    var quizAnswer3 = quiz.answer3;
-    var quizAnswer4 = quiz.answer4;
+  // Grabs the a random quiz question object made on line 33
+  var quiz = quizQuestions[quesCtr];
 
-    if (quizQues !== null) 
-    {
-        document.querySelector(".question").textContent = quizQues;
-        document.getElementById("answer1").textContent = quizAnswer1;
-        document.getElementById("answer2").textContent = quizAnswer2;
-        document.getElementById("answer3").textContent = quizAnswer3;
-        document.getElementById("answer4").textContent = quizAnswer4;
-    }
- }
+  // grabs variables from the object from the quizQuestions
+  // At this moment a quiz question and corresponding answers are loaded into the variables in lines
+  // 155 -159
+  var quizQues = quiz.question;
+  var quizNum = quiz.questionNum;
+  var quizAnswer1 = quiz.answer1;
+  var quizAnswer2 = quiz.answer2;
+  var quizAnswer3 = quiz.answer3;
+  var quizAnswer4 = quiz.answer4;
 
-function nextQuestion()
-{
-    renderMessage();
+  if (quizQues !== null) {
+    document.querySelector(".count").textContent = quizNum;
+    document.querySelector(".question").textContent = quizQues;
+    document.getElementById("answer1").textContent = quizAnswer1;
+    document.getElementById("answer2").textContent = quizAnswer2;
+    document.getElementById("answer3").textContent = quizAnswer3;
+    document.getElementById("answer4").textContent = quizAnswer4;
+  }
+  setCounter();
+}
+
+// visible.style.display will inhereit the display characteristic from it's parent
+
+function show() {
+  document.getElementById("visible").style.display = "inherit";
+}
+
+//  visible.style.display will hide everything within
+// <div class="card results" id="visible">
+
+function hide() {
+  document.getElementById("visible").style.display = "none";
 }
 
 
+function nextQuestion() {
 
+  renderMessage();
+}
 
+function correctAnswer() {
+  let quiz = quizQuestions[quesCtr];
+  correct = quiz.correctAnswer;
+  return correct;
+}
+
+function checkAnswer(answer) {
+  correct = quizQuestions[quesCtr].correctAnswer;
+
+  if (answer === correct) {
+    alert("That Is Correct!");
+    nextQuestion();
+  }
+  else
+  {
+    alert("That Is Incorrect.")
+    nextQuestion();
+  }
   
+
+}
+
 // Attach event listener to start button to call startGame function on click
 startButton.addEventListener("click", startGame);
 
+
 // Calls init() so that it fires when page opened
 init();
+hide();
 
 // when answering the question user will be brought to the next question in the quiz
-btn1.addEventListener("click", nextQuestion);
-btn2.addEventListener("click", nextQuestion);
-btn3.addEventListener("click", nextQuestion);
-btn4.addEventListener("click", nextQuestion);
+btn1.addEventListener ("click", function(event) {
+  var element = event.target;
+  var text=  element.textContent;
+
+  console.log(text);
+  checkAnswer(text);
+});
+
+btn2.addEventListener ("click", function(event) {
+  var element = event.target;
+  var text=  element.textContent;
+
+  console.log(text);
+  checkAnswer(text);
+});
+
+btn3.addEventListener ("click", function(event) {
+  var element = event.target;
+  var text=  element.textContent;
+
+  console.log(text);
+  checkAnswer(text);
+});
+
+btn4.addEventListener ("click", function(event) {
+  var element = event.target;
+  var text=  element.textContent;
+
+  console.log(text);
+  checkAnswer(text);
+});
 
